72cd637fe138b16a486269d94416c371e1aa7bdb
Thu Yein <thuyein1996alpha@gmail.com>
2025-06-12 10:47:13 +0700

    Show pet list in main page

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
+const pets = [ 
+
+  {"name": "Buddy", "type": "Dog", "age": 3, "img": "images/dogs/dog01.jpg"}, 
+
+  {"name": "Kylo", "type": "Dog", "age": 4, "img": "images/dogs/dog02.jpg"}, 
+
+  {"name": "John Doe", "type": "Dog", "age": 5, "img": "images/dogs/dog03.jpg"}, 
+
+  {"name": "Whiskers", "type": "Cat", "age": 2, "img": "images/cats/cat01.jpg"}, 
+
+  {"name": "Mittens", "type": "Cat", "age": 3, "img": "images/cats/cat02.jpg"},
+
+  {"name": "John Coe", "type": "Cat", "age": 4, "img": "images/cats/cat03.jpg"}, 
+
+];
+function loadPets(){
+    // Get the container
+    const petList = document.getElementById('pet-list');
+
+    // Loop through pets and add to the page
+    for (let i = 0; i < pets.length; i++) {
+        const pet = pets[i];
+        petList.innerHTML += `
+            <div class="pet">
+                <img src="${pet.img}" alt="Cute ${pet.type}">
+                <h3>${pet.name}</h3>
+                <p>Age: ${pet.age} years</p>
+                <p>${pet.type}</p>
+                <button onclick="adoptPet()">Adopt Now</button>
+            </div>
+        `;
+    }
+}
+
 function adoptPet() {
     alert("Thank you for your interest in adopting! Our team will contact you soon.");
 }
 
-const pets = [
-    {
-        name: "Charlie",
-        breed: "Golden Retriever",
-        image: "images/dog.jpg"
-    },
-    {
-        name: "Milo",
-        breed: "British Shorthair",
-        image: "images/cat.jpg"
-    }
-];
-
-// Get the container
-const petList = document.getElementById('pet-list');
-
-// Loop through pets and add to the page
-for (let i = 0; i < pets.length; i++) {
-    const pet = pets[i];
-    petList.innerHTML += `
-            <div class="pet">
-                <img src="${pet.image}" alt="Cute ${pet.breed}">
-                <p>${pet.name} - ${pet.breed}</p>
-                <button onclick="adoptPet()">Adopt Now</button>
-            </div>
-        `;
-}
+loadPets();
